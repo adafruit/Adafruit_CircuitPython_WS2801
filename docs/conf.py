d601fdcdeaa531ecb4c3762b33c2828005da2d4e
Kevin J Walters <kjw.github@gmail.com>
2018-03-17 14:15:41 +0000

    Fixing error from missing busio with autodoc_mock_imports in docs/conf.py - it no longer rains on me.

--- docs/conf.py
+++ docs/conf.py
@@ -16,12 +16,8 @@ extensions = [
     'sphinx.ext.todo',
 ]
 
-# TODO: Please Read!
-# Uncomment the below if you use native CircuitPython modules such as
-# digitalio, micropython and busio. List the modules you use. Without it, the
-# autodoc module docs will fail to generate with a warning.
-# autodoc_mock_imports = ["digitalio", "busio"]
-
+### Using busio
+autodoc_mock_imports = ["digitalio", "busio"]
 
 intersphinx_mapping = {'python': ('https://docs.python.org/3.4', None),'BusDevice': ('https://circuitpython.readthedocs.io/projects/bus_device/en/latest/', None),'CircuitPython': ('https://circuitpython.readthedocs.io/en/latest/', None)}
 
